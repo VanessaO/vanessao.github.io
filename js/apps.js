e82399da646f1c1defcfeb6ec3ca1f15f415e0bf
Vanessa Ogbuehi <va.ogbuehi@gmail.com>
2015-05-24 23:34:19 -0400

    Update apps.js

--- js/apps.js
+++ js/apps.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 
 	//Colorizes title and makes it responsive
 	$(".titleName").lettering().addClass('animated swing').fitText(0.8);
-
+	
 	$(".goDown").hover(
 		function(){
 			$(this).addClass('animated infinite bounce');
@@ -28,30 +28,6 @@ $(document).ready(function(){
     	captionsEl: '.caption'
     });
     
- /*  //Temporarily remove title attribute on hovered Slippry Images
-   $("#slippry-demo li").hover(function(){
- 
-        // Get the current title
-        var title = $(this).attr("title");
- 
-        // Store it in a temporary attribute
-        $(this).attr("tmp_title", title);
- 
-        // Set the title to nothing so we don't see the tooltips
-        $(this).attr("title","");
-         
-    });
- 
-  $(".sy-controls").click(function(){// Fired when we leave the element
- 
-        // Retrieve the title from the temporary attribute
-        var title = $("#slippry-demo li").attr("tmp_title");
- 
-        // Return the title to what it was
-        $("#slippry-demo li").attr("title", title);
-         
-    }); */
-
    //Enables smooth scrolling to each section
    $('a[href*=#]:not([href=#])').click(function() {
 	    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
